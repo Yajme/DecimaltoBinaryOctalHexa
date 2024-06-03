818393bf1975b038de201e4840ed0b661b6e7d07
Yajme <spartan.emjayrongavilla@gmail.com>
2024-06-03 11:59:50 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimaltoBinaryOctalHexa
+{
+    internal class Program
+    {
+        static Dictionary<string, int> NumberSet = new Dictionary<string, int>
+        {
+            { "Binary" , 2 },
+            { "Octal",8 },
+            { "Hexadecimal",16 }
+        };
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a decimal number: ");
+            try
+            {
+                int decimalNumber = int.Parse(Console.ReadLine());
+                foreach(var item in NumberSet)
+                {
+                    Console.WriteLine($"{item.Key} : {ConvertToBase(decimalNumber, item.Value)}");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        static string ConvertToBase(int number, int toBase)
+        {
+            if (toBase < 2 || toBase > 16)
+            {
+                throw new ArgumentException("The base must be between 2 and 16");
+            }
+
+            string result = string.Empty;
+            string characters = "0123456789ABCDEF";
+
+            do
+            {
+                int remainder = number % toBase;
+                result = characters[remainder] + result;
+                number /= toBase;
+            } while (number > 0);
+
+            return result;
+
+        }
+    }
+}
